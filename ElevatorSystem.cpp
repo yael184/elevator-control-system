9bc8b52bace38c103d6a3a147048aaab61ee0f3c
yael184 <yael56738@gmail.com>
2026-06-30 15:12:32 +0300

    Add project files.

---
+++ ElevatorSystem.cpp
@@ -0,0 +1,12 @@
+﻿// ElevatorSystem.cpp : Defines the entry point for the application.
+//
+
+#include "ElevatorSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
